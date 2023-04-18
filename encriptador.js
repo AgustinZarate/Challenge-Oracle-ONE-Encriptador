475ac9c53995b119bea3de7e7035e1460f28d801
Zarate Agustin <zarateagustin3@gmail.com>
2023-04-18 19:09:16 -0300

    Se agrega boton limpiador de textarea, notificacion usuario de copiado exitoso, arreglo bug al limpiar el texto encriptado, mejoras en la experiencia de usuario (captura enter - focus textarea)

--- encriptador.js
+++ encriptador.js
@@ -1,52 +1,4 @@
-/*
-¡Bienvenidas y bienvenidos a nuestro primer desafío!
-
-Durante estas cuatro semanas, vamos a trabajar en una aplicación que encripta textos, 
-así podrás intercambiar mensajes secretos con otras personas que sepan el secreto de la 
-encriptación utilizada.
-
-Las "llaves" de encriptación que utilizaremos son las siguientes:
-        La letra "e" es convertida para "enter"
-        La letra "i" es convertida para "imes"
-        La letra "a" es convertida para "ai"
-        La letra "o" es convertida para "ober"
-        La letra "u" es convertida para "ufat"
-
-Requisitos:
-    *Debe funcionar solo con letras minúsculas
-    *No deben ser utilizados letras con acentos ni caracteres especiales
-    *Debe ser posible convertir una palabra para la versión encriptada también devolver
-    una palabra encriptada para su versión original.
-
-Por ejemplo:
-    "gato" => "gaitober"
-    gaitober" => "gato"
-
-La página debe tener campos parainserción del texto que será encriptado o desencriptado, 
-y el usuario debe poder escoger entre as dos opciones.
-
-El resultado debe ser mostrado en la pantalla.
-
-Extras:
-    Un botón que copie el texto encriptado/desencriptado para la sección de transferencia,
-    o sea que tenga la misma funcionalidad del ctrl+C o de la opción "copiar" del menú de
-    las aplicaciones.
-
-Tenemos un periodo de tiempo de cuatro semanas para desarrollar el proyecto y vamos a 
-trabajar con el sistema ágil de desarrollo, utilizando el Trello de la siguiente forma:
-
-    *La columna Listos para iniciar presenta las tarjetas con elementos que aun no fueron desarrollados.
-    *En la columna En Desarrollo estarán los elementos que estés desarrollando en el momento. 
-    Al iniciar una tarea, podrás mover la tarjeta que contiene dicha tarea para esta columna.
-    *En la columna Pausado estarán los elementos que comenzaste a desarrollar, pero necesitaste 
-    parar por algún motivo.
-    *Por fin, en la columna Concluido estarán los elementos ya concluidos.
-    *El Trello es una herramienta de uso individual para que puedas controlar 
-    el progreso de tus actividades, pero no será evaluada.
-
-Buen proyecto!
-*/
-
+document.getElementById('textarea').focus();
 
 const encriptar = () => {
     document.getElementById('card-contenido').style.display = "none";
@@ -60,7 +12,23 @@ const encriptar = () => {
     textoEncriptado = textoEncriptado.replace(/o/img,'ober');
     textoEncriptado = textoEncriptado.replace(/u/img,'ufat');
     document.getElementById('texto-encriptado').innerHTML = textoEncriptado;
+    document.getElementById('btn-copiar').classList.remove("completado");
+    document.getElementById('btn-copiar').innerHTML = 'Copiar'
+    document.getElementById('textarea').focus();
+    
 }
+
+document.getElementById('textarea').addEventListener("keydown", (evento) => {
+    if (evento.keyCode === 13) {
+    evento.preventDefault();
+    }
+  });
+
+document.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        encriptar()
+    }
+});
 
 const desencriptar = () => {
     document.getElementById('card-contenido').style.display = "none";
@@ -74,18 +42,29 @@ const desencriptar = () => {
     textoDesencriptado = textoDesencriptado.replace(/ober/img,'o');
     textoDesencriptado = textoDesencriptado.replace(/ufat/img,'u');
     document.getElementById('texto-encriptado').innerHTML = textoDesencriptado;
-
+    document.getElementById('btn-copiar').classList.remove("completado");
+    document.getElementById('btn-copiar').innerHTML = 'Copiar'
+    document.getElementById('textarea').focus();
 }
 
 const copiar = () => {
     navigator.clipboard.writeText(document.getElementById('texto-encriptado').textContent)
+    document.getElementById('btn-copiar').classList.add("completado");
+    document.getElementById('btn-copiar').innerHTML = ''
+    document.getElementById('textarea').focus();
 }
 
 const limpiar = () => {
-    document.getElementById('texto-encriptado').value = ''
+    document.getElementById('texto-encriptado').innerHTML = ''
     document.getElementById('card-contenido').style.display = "flex";
     document.getElementById('texto-encriptado').style.display = "none";
     document.getElementById('btn-copiar').style.display = "none";
     document.getElementById('btn-limpiar').style.display = "none";
+    document.getElementById('btn-copiar').classList.remove('completado');
+    document.getElementById('btn-copiar').innerHTML = 'Copiar'
+    document.getElementById('textarea').focus();
+}
 
+const limpiarEncriptador = () =>{
+    document.getElementById('textarea').value = ''
 }
